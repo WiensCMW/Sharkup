e9a81f5026afdac59d5ef3c23ef63eaa3452f851
Cornie Wiens <cornie.wiens@vidir.com>
2024-08-14 17:04:47 -0500

    Playing around with FileInfo

--- Sharkup/Sharkup/Program.cs
+++ Sharkup/Sharkup/Program.cs
@@ -1,7 +1,26 @@
 ﻿internal class Program
 {
+    private static string _sourcePath = @"C:\Users\cornie\OneDrive - Vidir Solutions Inc\Desktop";
+    private static string _stagingPath = @"C:\Users\cornie\Downloads\Sharkup\Staging";
+    private static string _destinationPath = @"C:\Users\cornie\Downloads\Sharkup\Backups";
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to Sharkup!");
+
+        DirectoryInfo sourceDir = new DirectoryInfo(_sourcePath);
+        DirectoryInfo stagingDr = new DirectoryInfo(_sourcePath);
+        DirectoryInfo destinationDir = new DirectoryInfo(_sourcePath);
+
+        FileInfo[] sourceFiles = sourceDir.GetFiles("*", SearchOption.AllDirectories);
+        DirectoryInfo[] sourceFolders = sourceDir.GetDirectories("*", SearchOption.AllDirectories);
+        Dictionary<string, DirectoryInfo> _sourceFoldersDict = new Dictionary<string, DirectoryInfo>();
+
+        for (int i = 0; i < sourceFolders.Length; i++)
+        {
+            _sourceFoldersDict.Add(Path.GetRelativePath(sourceDir.FullName, sourceFolders[i].FullName), sourceFolders[i]);
+        }
+
+        Console.ReadKey();
     }
 }
